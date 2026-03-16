001ed1befa13baa43ee694d288155ac60828f005
Aaru-coder <ay6049453@gmail.com>
2026-03-16 16:41:20 +0530

    change of the certificat

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
     issueDate: "06/04/2026",
     signer: "ASHOK KHANDELWAL",
     signDate: "2026.04.06 14:12:42 IST",
-    imageUrl: "https://res.cloudinary.com/dxlw574rm/image/upload/v1773572849/WhatsApp_Image_2026-03-15_at_1.06.06_PM_z5g4hy.jpg" 
+    imageUrl: "https://res.cloudinary.com/dpjwd6oae/image/upload/v1773659355/certificate_1_uekjwn.jpg" 
   };
 
   return <Certificate data={studentData} />;
